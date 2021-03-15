3eee6299b5d58ea2afa1da9878f3b80854376e59
yaroslav-tormasov <yaroslav606@gmail.com>
2021-03-15 21:12:34 +0400

    components 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,36 +8,54 @@ function App() {
     // обязана  вернуть jsx
     return (
         <div>
-            This is APP component
+            <Title/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
 }
 
 function Star() {
-  return <div>
-    star
-  </div>
+    return <div>star</div>
 }
 
 function Rating() {
     return (
         <div>
-            <Star />
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
         </div>
     )
 }
 
 function Accordion() {
     return <div>
-        <h3>Меню</h3>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
+        <AccordionTitle/>
+        <AccordionBody/>
     </div>
 }
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+    return <h3>Меню</h3>
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+}
+
+function Title() {
+    return <>"This is APP component"</>
+}
+
 
 export default App;
